14acc09106e0df19fea87163fe9e8562c61428d8
orville94Bailey <orville94Bailey@users.noreply.github.com>
2017-10-08 12:55:35 -0400

    base commit

---
+++ ConsoleTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleAccessTesting.ControlCursor();
+        }
+    }
+}
